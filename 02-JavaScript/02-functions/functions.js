945612fec483a81aede07917402fb1a7069fa23e
Legion <bobyzz@mail.ru>
2025-08-08 11:23:48 +0300

    SwitchBackground with transition DONE

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -14,19 +14,25 @@ function Power(base, exponent)
 function SwitchBackground()
 {
     let switchButton = document.getElementById('switchBackground');
-    console.log(switchButton.attributes.src);
+    //console.log(switchButton.attributes.src);
     //switchButton.attributes.src.nodeValue =
     //    switchButton.attributes.src.nodeValue == 'img/moon.png' ? 'img/sun.png' : 'img/moon.png';
-    if (switchButton.attributes.src.nodeValue == 'img/moon.png')
-    {
-        switchButton.attributes.src.nodeValue = 'img/sun.png';
-        document.body.style.background = "#323232";
-        document.body.style.color = "white";
-    }
-    else 
-    {
-        switchButton.attributes.src.nodeValue = 'img/moon.png';
-        document.body.style.background = "#FFFFFF";
-        document.body.style.color = "black";
-    }
+
+    //if (switchButton.attributes.src.nodeValue == 'img/moon.png')
+    //{
+    //    switchButton.attributes.src.nodeValue = 'img/sun.png';
+    //    document.body.style.background = "#323232";
+    //    document.body.style.color = "white";
+    //}
+    //else
+    //{
+    //    switchButton.attributes.src.nodeValue = 'img/moon.png';
+    //    document.body.style.background = "#FFFFFF";
+    //    document.body.style.color = "black";
+    //}
+    let delay = Number(document.getElementById("delay").value);
+    console.log(delay);
+    document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
+    document.getElementById(`switchBackground`).style.transition = `background-image ${delay}s`;
+    document.body.className = document.body.className == "light" ? "dark" : "light";
 }
