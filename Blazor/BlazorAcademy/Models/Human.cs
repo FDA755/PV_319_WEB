97203e9a2bcaf18dca0184f4606203843e7e6a9d
Legion <bobyzz@mail.ru>
2025-09-26 23:17:00 +0300

    Before Student scaffolding

---
+++ Blazor/BlazorAcademy/Models/Human.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAcademy.Models
+{
+	public class Human
+	{
+		[Required]
+		public string last_name { get; set; }
+		[Required]
+		public string first_name { get; set; }
+		public string? middle_name { get; set; }
+		[Required]
+		DateTime birtDate { get; set; }
+		[EmailAddress]
+		public string? email { get; set; }
+		[Phone]
+		public string? phone { get; set; }
+
+		public byte[]? photo { get; set; }
+		
+
+	}
+}
